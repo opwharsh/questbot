c1a6a11a974e7a7b10086567f62b69f5d09b7efb
opwharsh <45063@arsd.du.ac.in>
2026-09-23 00:08:13 +0530

    Update status.js

--- status.js
+++ status.js
@@ -1,16 +1,35 @@
+const statuses = [
+  "ᴋᴇᴇᴘ ꜰᴀʟʟɪɴɢ ɪɴ ʟᴏᴠᴇ ᴡɪᴛʜ ʟᴀɴᴀ ᴅᴇʟ ʀᴇʏ",
+  "ᴍᴏɢɢɪɴɢ ɪꜱ ᴀ ꜱɪᴅᴇ ʜᴜꜱᴛʟᴇ ꜰᴏʀ ᴊᴏʀᴅᴀɴ ʙᴀʀʀᴇᴛᴛ",
+  "ᴢᴀʏɴ ᴍᴀʟɪᴋ ɪꜱ ᴛʜᴇ ɢᴏᴀᴛ",
+  "ᴊᴜꜱᴛɪɴɴɴɴɴ ɪꜱ ᴛʜᴇ ᴍᴏᴏᴅ"
+];
+
 async function setStatus(client) {
-  await client.updatePresence(0, {
-    since: null,
-    activities: [
-      {
-        name: "custom",
-        type: 4,
-        state: "ʙᴇ ᴀ ᴍᴀɴ, ɴᴏᴛ ᴀ ʙᴀsᴛᴀʀᴅ."
-      }
-    ],
-    status: "idle",
-    afk: false
-  });
+  let index = 0;
+
+  const update = async () => {
+    await client.updatePresence(0, {
+      since: null,
+      activities: [
+        {
+          name: "custom",
+          type: 4,
+          state: statuses[index]
+        }
+      ],
+      status: "idle",
+      afk: false
+    });
+
+    index = (index + 1) % statuses.length;
+  };
+
+  // Set first status immediately
+  await update();
+
+  // Change status every 10 seconds
+  setInterval(update, 10_000);
 }
 
 module.exports = { setStatus };
