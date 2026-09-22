ee47e50aa8fbbedaa32fee806be6a95276a3e619
opwharsh <45063@arsd.du.ac.in>
2026-09-23 00:24:27 +0530

    Fix API call to delete guild on leave

--- leaveguild.ts
+++ leaveguild.ts
@@ -26,7 +26,7 @@ export async function leaveGuildCommand(
     }
 
     try {
-        await api.users.guilds.delete(guildId);
+        await api.guilds.delete(guildId);
 
         await api.channels.createMessage(message.channel_id, {
             content: `✅ Successfully left the server \`${guildId}\`.`,
